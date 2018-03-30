358ee7bb1b23e3c9d39e9bc550a840d514dfedf8
unknown <016713@senao.com.tw>
2018-03-30 14:07:28 +0800

    complete section 4

--- section-4/code/app.py
+++ section-4/code/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse
 from flask_jwt import JWT, jwt_required
 
 from security import authenticate, identity
@@ -9,8 +9,10 @@ app = Flask(__name__)
 app.secret_key = 'ken'
 api = Api(app)
 
+# JWT creates a new endpoint, that endpoint is /auth , when we call /auth we send a username & password, and JWT send to authenticate fuction 
+# we can send it to the next request when we send JWT token what JWT does is calls the identity function, and it use JWT token to get user ID 
 
-jwt = JWT(app, authenticate, identity) # /auth
+jwt = JWT(app, authenticate, identity) # /auth  (object of a secret, authenticate, payload)
 
 #Create a in-memory database
 items = []
@@ -18,6 +20,16 @@ items = []
 
 #Define our resource
 class Item(Resource):
+
+	#be aware of these aren't instance variabe instead these are class variable
+	parser = reqparse.RequestParser()
+	parser.add_argument('price',
+				type=float,
+				required = True,
+				help = "This filed cannot be left blank!"
+			)
+	data = parser.parse_args()
+
 	#@app.route('/item/<string:name>')
 	@jwt_required()
 	def get(self, name):
@@ -29,18 +41,53 @@ class Item(Resource):
 		item = next(filter(lambda x: x['name'] == name, items),None)
 		return {'item': item}, 200 if item else 404
 
-
+	 
 	def post(self, name):
 		if next(filter(lambda x: x['name'] == name, items), None) is not None:
 			return {'message': 'The item is already esists.'} , 400
 
-		data = request.get_json(force=True) #if not proper header content type will cause error or if it's not json format
+		#data = request.get_json(force=True) #if not proper header content type will cause error or if it's not json format, force means  no need content type header
+		# parser = reqparse.RequestParser()
+		# parser.add_argument('price',
+		# 		type=float,
+		# 		required = True,
+		# 		help = "This filed cannot be left blank!"
+		# 	)
+		# data = parser.parse_args()
+		data = Item.parser.parse_args()
 		item = {'name': name, 'price': data['price']}
 		items.append(item)
 		return item , 201
 
 	def delete(self, name):
-		pass
+		#declare the item variable is the "items" out oh the class
+		global items
+		items = list(filter(lambda x: x['name'] != name, items))
+		return {'message': 'Item deleted'}
+
+	def put(self, name):
+		#data = request.get_json(force=True)
+		# parser = reqparse.RequestParser()
+		# parser.add_argument('price',
+		# 		type=float,
+		# 		required = True,
+		# 		help = "This filed cannot be left blank!"
+		# 	)
+		# data = parser.parse_args()
+		#print(data['another'])
+		
+		data = Item.parser.parse_args()
+		item = next(filter(lambda x:x['name'] == name, items), None)
+		if item is None:
+			item = {'name': name, 'price': data['price']}
+			items.append(item)
+		else:
+			#use entire payload, if payload had a name that will change entire row 
+			#item.update(data)
+		return item
+
+
+
 
 class ItemList(Resource):
 	def get(self):
