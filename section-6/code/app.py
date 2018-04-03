20c75f95a1718a33b1b2429fb8e751c398667985
unknown <016713@senao.com.tw>
2018-04-03 16:35:44 +0800

    half way to go...

--- section-6/code/app.py
+++ section-6/code/app.py
@@ -3,12 +3,23 @@ from flask_restful import Api
 from flask_jwt import JWT
 
 from security import authenticate, identity
-from user import UserRegister
-from item import Item, ItemList
+from resources.user import UserRegister
+from resources.item import Item, ItemList
+
+
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False #tracking every modification (turn off)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+
 app.secret_key = 'jose'
 api = Api(app)
+
+##Bulid the database automatically
+@app.before_first_request
+def create_tables():
+	db.create_all()
 
 jwt = JWT(app, authenticate, identity)
 
@@ -17,4 +28,6 @@ api.add_resource(ItemList, '/items')
 api.add_resource(UserRegister, '/register')
 
 if __name__ == '__main__':
-    app.run(debug=True)  # important to mention debug=True
+	from db import db #must to import here 
+	db.init_app(app)
+	app.run(port=8080, debug = True)  # important to mention debug=True
