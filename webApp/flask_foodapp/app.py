8cc2fee5a6fd41aaccae71d4d6c6be881d213338
unknown <016713@senao.com.tw>
2018-04-11 15:38:55 +0800

    bulid my own food app

--- webApp/flask_foodapp/app.py
+++ webApp/flask_foodapp/app.py
@@ -1,20 +1,123 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect , g #Just store on this whatever you want. For example a database connection or the user that is currently logged in
+from flask_sqlalchemy import SQLAlchemy
+import sqlite3
+
 
 app = Flask(__name__)
+'''
+good practice of database
+'''
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False #tracking every modification (turn off)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///food_track.db'
+
+db = SQLAlchemy(app)
+
+class log_date(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	entry_date = db.Column(db.String(255))
+
+
+	def __repr__(self):
+		return 'logdate'
+
+	def insert_data_row(self):
+		db.session.add(self)
+		db.session.commit()
+		# a = log_date(id = 1, entry_date = '2018/04/11')
+
+	def delete_data_row(self):
+		db.session.delete(self)
+		db.session.commit()
+
+	@classmethod
+	def query_data_row(cls, id):
+		result = cls.query.filter_by(id=id).first()
+		if result:
+			return 'The date of '+ str(result.id) +' is '+result.entry_date
+		else:
+			return 'This row is not exist...'
+
+	@classmethod
+	def show_all_rows(cls):
+		results = cls.query.all()
+		return 'Total rows count is: '+ str(len(results))
+
+
+
+
+class food(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	name = db.Column(db.String(255))
+	protein = db.Column(db.String(30))
+	carbohydrates = db.Column(db.String(30))
+	fat = db.Column(db.String(30))
+	calories = db.Column(db.String(30))
+
+	def __repr__(self):
+		return 'food'
+
+	def insert_data_row(self):
+		db.session.add(self)
+		db.session.commit()
+
+	@classmethod
+	def show_all_rows(cls):
+		results = cls.query.all()
+		return 'Total rows count is: '+ str(len(results))
+
+class food_date(db.Model):
+	food_id = db.Column(db.Integer, primary_key=True)
+	log_date_id = db.Column(db.Integer, primary_key=True)
+
+
+	def __repr__(self):
+		return 'food_date'
+'''
+from app import db
+db.create_all()
+'''
+
+'''
+Bad practice of database
+'''
+def connect_db():
+	sql = sqlite3.connect("D:\\016713\Desktop\\python pratice\\2018_FlaskProject\\webApp\\flask_foodapp\\food_track.db")
+	sql.row_factory = sqlite3.Row
+	return sql
+
+def get_db():
+	if not hasattr(g, 'sqlite3'):
+		g.sqlite_db = connect_db()
+	return g.sqlite_db
+
+@app.teardown_appcontext
+def close_db(error):
+	if hasattr(g, 'sqlite_db'):
+		g.sqlite_db.close()
+
 
 
 @app.route('/')
 def home():
 	return render_template('home.html')
 
-@app.route('/addfood')
+@app.route('/addfood', methods=['GET','POST'])
 def add_food():
+	if request.method == 'POST':
+		name = request.form['food_name']
+		protein = request.form['protein']
+		carbohydrates = request.form['carbohydrates']
+		fat = request.form['fat']
+		new_food = food(name=name,protein=protein,carbohydrates=carbohydrates,fat=fat)
+		new_food.insert_data_row()	
+		return '<h1> Food added!! </h1>'
+
 	return render_template('add_food.html')
 
 @app.route('/day')
 def day():
 	return render_template('day.html')
-
+ 
 
 
 if __name__ == '__main__':
